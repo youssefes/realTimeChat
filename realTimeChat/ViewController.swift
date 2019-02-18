6f6551e196a5d5775685c96e8a923ea67e5658c8
youssef <youssef.esmaileffekygmail.com@youssefs-mac.local>
2019-02-18 21:36:40 +0200

    Initial Commit

---
+++ realTimeChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  realTimeChat
+//
+//  Created by youssef on 2/18/19.
+//  Copyright © 2019 youssef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
